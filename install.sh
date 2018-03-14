1d6b7e7e95206e8391ff409f94e58e99d75a42b8
Arvid Gerstmann <dev@arvid-g.de>
2018-03-14 12:38:17 +0100

    Fix install script, when no UltiSnips directory is present yet

--- install.sh
+++ install.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
 if [[ "$(uname)" =~ MINGW64.* ]]; then
-    cp -r $HOME/vimfiles/UltiSnips .backup
+    if [ -d "$HOME/vimfiles/UltiiSnips" ]; then
+        cp -r $HOME/vimfiles/UltiSnips .backup
+    fi
     cp -R UltiSnips $HOME/vimfiles/UltiSnips
 else
-    cp -r $HOME/.vim/UltiSnips .backup
+    if [ -d "$HOME/.vim/UltiiSnips" ]; then
+        cp -r $HOME/.vim/UltiSnips .backup
+    fi
     cp -R UltiSnips $HOME/.vim/UltiSnips
 fi
